4186f187c58cd93848ab160d9bec4fd447f0bfc1
ProgrammerSteven <Stevenmb1995@gmail.com>
2021-12-31 02:19:26 -0600

    blah blah

--- server.js
+++ server.js
@@ -25,14 +25,9 @@ const image = require('./controllers/image');
 
 //process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 
-const db=knex({
-	client:'pg',
-	connection:{
-		connectionString:process.env.DATABASE_URL,
-		ssl: {
-      		rejectUnauthorized: false
-    	}
-	}
+const db = knex({
+  client: "pg",
+  connection: process.env.DATABASE_URL
 });
 
 
